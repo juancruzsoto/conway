6d9b51c1dea25210804c46355470bc00944098cd
Juan Cruz Soto <juancruzlp2013@gmail.com>
2022-06-29 14:24:56 -0300

    states management improved

--- src/components/Home/CellGrid.jsx
+++ src/components/Home/CellGrid.jsx
@@ -1,85 +1,100 @@
 import React, { useEffect, useState } from "react";
-import CircleRoundedIcon from "@mui/icons-material/CircleRounded";
-import CircleTwoToneIcon from "@mui/icons-material/CircleTwoTone";
+// import CircleRoundedIcon from "@mui/icons-material/CircleRounded";
+// import CircleTwoToneIcon from "@mui/icons-material/CircleTwoTone";
 import CircleOutlinedIcon from "@mui/icons-material/CircleOutlined";
 import { Grid, IconButton } from "@mui/material";
 import "./css/cellGrid.css";
 
 const CellGrid = () => {
-  const [cellsState, setCellsState] = useState({});
+  // const [cellsState, setCellsState] = useState({});
   const [cellsInGridState, setCellsInGridState] = useState([]);
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
 
-  const handleChangeState = (x, i) => {
-    setCellsState({ ...cellsState, [`${x},${i}`]: true });
-    // let updatedState = cellsState;
-    // updatedState[`${x},${i}`] = !updatedState[`${x},${i}`];
-
-    // setCellsState(updatedState);
+  const handleResize = () => {
+    setDimensions({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
   };
 
-  const putRow = (x, cells) => {
-    let row = [];
-    for (let i = 0; i < 50; i++) {
-      cells[`${x},${i}`] = true;
-      row.push(
-        <IconButton
-          aria-label=""
-          key={`${x},${i}`}
-          component="span"
-          value={[x, i]}
+  const handleChangeState = (x, i, setState) => {
+    // document.getElementById(`${0},${0}`).click();
+    setState((currentState) => !currentState);
+  };
+
+
+
+  console.log(dimensions.height, dimensions.height);
+
+  const Circle = (props) => {
+    const [state, setState] = useState(false);
+
+    return (
+      <IconButton
+        aria-label=""
+        id={`${props.x},${props.i}`}
+        component="span"
+        style={{
+          color: "black",
+          backgroundColor: state && "lightblue",
+          padding: "0px",
+          marginTop: "5px",
+          marginRight: "11px",
+        }}
+        onClick={() => handleChangeState(props.x, props.i, setState)}
+      >
+        <CircleOutlinedIcon
           style={{
-            color: "black",
-            backgroundColor: cellsState[`${x},${i}`] && "lightblue",
-            padding: "0px",
-            marginTop: "5px",
-            marginRight: "11px",
+            fontSize:
+              (dimensions.height - (props.h * 5 + 100)) / props.h >
+              (dimensions.width - (props.w * 11 + 40)) / props.w
+                ? (dimensions.width - (props.w * 11 + 40)) / props.w
+                : (dimensions.height - (props.h * 5 + 100)) / props.h,
           }}
-          onClick={() => handleChangeState(x, i)}
-        >
-          {console.log("AAAAAAAAA", cellsState[`${x},${i}`])}
-          <CircleOutlinedIcon style={{ fontSize: 13 }} />
-        </IconButton>
-      );
-    }
-    return row;
+        />
+      </IconButton>
+    );
   };
 
   const putAllRows = () => {
     let rows = [];
-    let cells = {};
-    for (let x = 0; x < 30; x++) {
-      rows.push(
-        <Grid item xs={12}>
-          {putRow(x, cells)}
-        </Grid>
-      );
+    let h = 30;
+    let w = 50;
+    for (let x = 0; x < h; x++) {
+      let row = [];
+      for (let i = 0; i < w; i++) {
+        row.push(<Circle h={h} w={w} x={x} i={i} />);
+      }
+      rows.push(row);
     }
-    console.log(cells);
-
-    return { cells, rows };
+    return rows;
   };
 
   useEffect(() => {
-    let { cells, rows } = putAllRows();
-    setCellsState(cells);
-    setCellsInGridState(rows);
+    window.addEventListener("resize", handleResize, false);
   }, []);
+
+  useEffect(() => {
+    setCellsInGridState(putAllRows());
+  }, [dimensions]);
 
   return (
     <div className="cellGrid">
-      {/* <IconButton color="primary" aria-label="" component="span" onClick={() => console.log("hola")}>
-        <CircleRoundedIcon  />
-      </IconButton> */}
-      {/* <IconButton aria-label="" component="span" style={{color:"gray", padding:"0px"}}>
-        <CircleTwoToneIcon />
-      </IconButton> */}
       <Grid
         container
         direction="column"
         justifyContent="flex-start"
         alignItems="center"
       >
-        {cellsInGridState}
+        {cellsInGridState &&
+          cellsInGridState.map((row, index) => (
+            <Grid item xs={12}>
+              {row.map((r, i) => r)}
+            </Grid>
+          ))}
       </Grid>
     </div>
   );
